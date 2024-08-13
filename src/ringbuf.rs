5e011af1cdf748024907ece0c49f95898c57d4b3
Vincent Sparks <vince@spacestation.blue>
2024-08-13 01:02:06 -0700

    fixed a glaring bug in the do_not_consume() implementation -- which i cannot seem to make that unit test catch. something to do tomorrow.

--- src/ringbuf.rs
+++ src/ringbuf.rs
@@ -37,7 +37,8 @@ type RingBufGuard<'a,T> = MutexGuard<'a, RingBufInner<T>>;
 
 pub struct RingBufSlot<'a, T, const Writable: bool> {
     guard: RingBufGuard<'a, T>,
-    slot: usize,
+    pub slot: usize,
+    caused_wrap: bool,
 }
 
 impl<'a, T, const Writable: bool> Deref for RingBufSlot<'a, T, Writable> {
@@ -80,22 +81,31 @@ impl<'a, T, const WRITE: bool> RingBufSlot<'a,T,WRITE> {
     pub fn do_not_consume(mut self) {
         if WRITE {
             self.guard.write_offset = self.slot;
+            if self.caused_wrap {
+                debug_assert!(self.guard.writer_wrapped);
+                self.guard.writer_wrapped=false;
+            }
         } else {
             self.guard.read_offset = self.slot;
+            if self.caused_wrap {
+                debug_assert!(!self.guard.writer_wrapped);
+                self.guard.writer_wrapped=true;
+            }
         }
     }
 }
 
 fn try_get_slot<'a, T, const WRITE: bool>(mut guard: RingBufGuard<'a, T>) -> Result<RingBufSlot<'a, T, WRITE>, RingBufGuard<'a, T>> {
     let can_advance = guard.read_offset != guard.write_offset || (WRITE ^ guard.writer_wrapped);
-    dbg!(guard.read_offset, guard.write_offset, guard.writer_wrapped, WRITE, can_advance);
     if can_advance {
+        let mut caused_wrap=false;
         let slot = if WRITE {
             let slot = guard.write_offset;
             guard.write_offset += 1;
             if guard.write_offset >= guard.buffer.len() {
                 guard.write_offset = 0;
                 guard.writer_wrapped = true;
+                caused_wrap=true;
             }
             slot
         } else {
@@ -104,10 +114,11 @@ fn try_get_slot<'a, T, const WRITE: bool>(mut guard: RingBufGuard<'a, T>) -> Res
             if guard.read_offset >= guard.buffer.len() {
                 guard.read_offset = 0;
                 guard.writer_wrapped = false;
+                caused_wrap=true;
             }
             slot
         };
-        Ok(RingBufSlot{guard, slot})
+        Ok(RingBufSlot{guard, slot, caused_wrap})
     } else {
         Err(guard)
     }
@@ -143,7 +154,11 @@ impl<T> RingBuf<T> {
         let mut guard = self.inner.lock().unwrap();
         loop {
             guard = match try_get_slot::<T, false>(guard) {
-                Ok(res) => return Ok(res),
+                Ok(res) => {
+                    // TODO separate read and write condvars.
+                    self.condvar.notify_one();
+                    return Ok(res)
+                },
                 Err(guard) => {
                     if guard.writer_closed {
                         return Err(AcquireError::ChannelClosed);
@@ -158,9 +173,13 @@ impl<T> RingBuf<T> {
     pub fn try_read(&self) -> Result<RingBufSlot<'_,T,false>,TryAcquireError> {
         let guard = self.inner.lock().unwrap();
 
-        try_get_slot::<T, false>(guard).map_err(|guard| 
+        let r = try_get_slot::<T, false>(guard).map_err(|guard| 
             if guard.writer_closed {TryAcquireError::ChannelClosed} else {TryAcquireError::NotReady}
-            )
+            );
+        if r.is_ok() {
+            self.condvar.notify_one();
+        }
+        r
     }
 
     pub fn write(&self) -> Result<RingBufSlot<'_,T,true>,AcquireError> {
@@ -170,7 +189,10 @@ impl<T> RingBuf<T> {
                 return Err(AcquireError::ChannelClosed);
             }
             guard = match try_get_slot::<T, true>(guard) {
-                Ok(res) => return Ok(res),
+                Ok(res) => {
+                    self.condvar.notify_one();
+                    return Ok(res)
+                },
                 Err(guard) => self.condvar.wait(guard).unwrap(),
             }
         }
@@ -182,7 +204,18 @@ impl<T> RingBuf<T> {
             return Err(TryAcquireError::ChannelClosed);
         }
 
-        try_get_slot(guard).map_err(|_| TryAcquireError::NotReady)
+        let r = try_get_slot(guard).map_err(|_| TryAcquireError::NotReady);
+        if r.is_ok() {
+            self.condvar.notify_one();
+        }
+        r
+    }
+
+    pub fn close_read(&self) {
+        self.inner.lock().unwrap().reader_closed = true;
+    }
+    pub fn close_write(&self) {
+        self.inner.lock().unwrap().writer_closed = true;
     }
 }
 
@@ -213,6 +246,9 @@ mod test {
         assert!(ringbuf.try_write().is_err());
         assert_eq!(*ringbuf.try_read().unwrap(), 1);
         assert_eq!(*ringbuf.try_read().unwrap(), 2);
+        let mut s = ringbuf.try_write().unwrap();
+        *s = 999;
+        s.do_not_consume();
         *ringbuf.try_write().unwrap() = 4;
         *ringbuf.try_write().unwrap() = 5;
         assert_eq!(*ringbuf.try_read().unwrap(), 3);
@@ -221,6 +257,16 @@ mod test {
         s.do_not_consume();
         assert_eq!(*ringbuf.try_read().unwrap(), 4);
         assert_eq!(*ringbuf.try_read().unwrap(), 5);
+        assert!(ringbuf.try_read().is_err());
+    }
+    
+    #[test]
+    fn test_ringbuf_end() {
+        let ringbuf = RingBuf::new(3,||0);
+        assert!(ringbuf.try_write().is_ok());
+        assert!(ringbuf.try_read().is_ok());
+        assert!(ringbuf.try_write().is_ok());
+        assert!(ringbuf.try_read().is_ok());
         assert!(ringbuf.try_read().is_err());
     }
 }
