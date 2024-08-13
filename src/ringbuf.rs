1932d0cb97f1e0702f5e59be099d66f823406853
Vincent Sparks <vince@spacestation.blue>
2024-08-12 19:56:12 -0700

    made ringbuf generic

--- src/ringbuf.rs
+++ src/ringbuf.rs
@@ -3,8 +3,8 @@ use std::ops::{Deref,DerefMut};
 
 use ffmpeg_the_third::Frame;
 
-struct RingBufInner {
-    buffer: Box<[Frame]>,
+struct RingBufInner<T> {
+    buffer: Box<[T]>,
     read_offset: usize,
     write_offset: usize,
     writer_wrapped: bool,
@@ -12,15 +12,15 @@ struct RingBufInner {
     writer_closed: bool,
 }
 
-impl RingBufInner {
-    fn new(size: usize) -> Self {
+impl<T> RingBufInner <T>{
+    fn new(size: usize, mut init: impl FnMut()->T) -> Self {
         // an unsafe method call and TWO unstable feature flags.
         // there has GOT to be a better way to initialize an array with multiple function calls!
         //
         // TODO badger the Rust developers to make this part of the standard library.
         let mut buffer = Box::new_uninit_slice(size);
         for e in buffer.iter_mut() {
-            e.write(unsafe {Frame::empty()});
+            e.write(init());
         }
         Self {
             buffer: unsafe {buffer.assume_init()},
@@ -33,34 +33,49 @@ impl RingBufInner {
     }
 }
 
-type RingBufGuard<'a> = MutexGuard<'a, RingBufInner>;
+type RingBufGuard<'a,T> = MutexGuard<'a, RingBufInner<T>>;
 
-pub struct RingBufSlot<'a, const Writable: bool> {
-    guard: RingBufGuard<'a>,
+pub struct RingBufSlot<'a, T, const Writable: bool> {
+    guard: RingBufGuard<'a, T>,
     slot: usize,
 }
 
-impl<'a, const Writable: bool> Deref for RingBufSlot<'a, Writable> {
-    type Target=Frame;
-    fn deref(&self)->&Frame {
+impl<'a, T, const Writable: bool> Deref for RingBufSlot<'a, T, Writable> {
+    type Target=T;
+    fn deref(&self)->&T {
         &self.guard.buffer[self.slot]
     }
 }
 
-impl<'a> DerefMut for RingBufSlot<'a, true> {
-    fn deref_mut(&mut self)->&mut Frame {
+impl<'a, T> DerefMut for RingBufSlot<'a, T, true> {
+    fn deref_mut(&mut self)->&mut T {
         &mut self.guard.buffer[self.slot]
     }
 }
 
-impl<'a> RingBufSlot<'a,true> {
+impl<'a, T> RingBufSlot<'a, T,true> {
+    /*
+    pub fn pts_sift_down(&mut self) {
+        // TODO finish this method.
+        let mut i = self.slot;
+        
+        // SAFETY: the following code cannot panic.
+        loop {
+            let prev_idx = if i == 0 && self.guard.writer_wrapped {
+                self.guard.buffer.len()-1
+            } else {i-1};
+            if prev_idx == self.guard.read_offset {break;}
+            
+        }
+    }
+    */
 }
 
-fn try_get_slot<'a, const write: bool>(mut guard: RingBufGuard<'a>) -> Result<RingBufSlot<'a,write>, RingBufGuard<'a>> {
-    let can_advance = guard.read_offset != guard.write_offset || (write ^ guard.writer_wrapped);
-    dbg!(guard.read_offset, guard.write_offset, guard.writer_wrapped, write, can_advance);
+fn try_get_slot<'a, T, const WRITE: bool>(mut guard: RingBufGuard<'a, T>) -> Result<RingBufSlot<'a, T, WRITE>, RingBufGuard<'a, T>> {
+    let can_advance = guard.read_offset != guard.write_offset || (WRITE ^ guard.writer_wrapped);
+    dbg!(guard.read_offset, guard.write_offset, guard.writer_wrapped, WRITE, can_advance);
     if can_advance {
-        let slot = if write {
+        let slot = if WRITE {
             let slot = guard.write_offset;
             guard.write_offset += 1;
             if guard.write_offset >= guard.buffer.len() {
@@ -83,34 +98,36 @@ fn try_get_slot<'a, const write: bool>(mut guard: RingBufGuard<'a>) -> Result<Ri
     }
 }
 
-pub struct RingBuf {
-    inner: Mutex<RingBufInner>,
+pub struct RingBuf<T> {
+    inner: Mutex<RingBufInner<T>>,
     condvar: Condvar,
 }
 
-impl RingBuf {
-    fn new(size: usize) -> Self {
+impl<T> RingBuf<T> {
+    pub fn new(size: usize, init: impl FnMut()->T) -> Self {
         Self {
-            inner: Mutex::new(RingBufInner::new(size)),
+            inner: Mutex::new(RingBufInner::new(size, init)),
             condvar: Condvar::new(),
         }
     }
 }
 
+#[derive(Debug)]
 pub enum TryAcquireError {
     ChannelClosed,
     NotReady,
 }
 
+#[derive(Debug)]
 pub enum AcquireError {
     ChannelClosed,
 }
 
-impl RingBuf {
-    pub fn read(&self) -> Result<RingBufSlot<'_,false>,AcquireError> {
+impl<T> RingBuf<T> {
+    pub fn read(&self) -> Result<RingBufSlot<'_,T,false>,AcquireError> {
         let mut guard = self.inner.lock().unwrap();
         loop {
-            guard = match try_get_slot::<false>(guard) {
+            guard = match try_get_slot::<T, false>(guard) {
                 Ok(res) => return Ok(res),
                 Err(guard) => {
                     if guard.writer_closed {
@@ -123,34 +140,34 @@ impl RingBuf {
         }
     }
 
-    pub fn try_read(&self) -> Result<RingBufSlot<'_,false>,TryAcquireError> {
+    pub fn try_read(&self) -> Result<RingBufSlot<'_,T,false>,TryAcquireError> {
         let guard = self.inner.lock().unwrap();
 
-        try_get_slot::<false>(guard).map_err(|guard| 
+        try_get_slot::<T, false>(guard).map_err(|guard| 
             if guard.writer_closed {TryAcquireError::ChannelClosed} else {TryAcquireError::NotReady}
             )
     }
 
-    pub fn write(&self) -> Result<RingBufSlot<'_,true>,AcquireError> {
+    pub fn write(&self) -> Result<RingBufSlot<'_,T,true>,AcquireError> {
         let mut guard = self.inner.lock().unwrap();
         loop {
             if guard.reader_closed {
                 return Err(AcquireError::ChannelClosed);
             }
-            guard = match try_get_slot::<true>(guard) {
+            guard = match try_get_slot::<T, true>(guard) {
                 Ok(res) => return Ok(res),
                 Err(guard) => self.condvar.wait(guard).unwrap(),
             }
         }
     }
 
-    pub fn try_write(&self) -> Result<RingBufSlot<'_,true>,TryAcquireError> {
+    pub fn try_write(&self) -> Result<RingBufSlot<'_,T,true>,TryAcquireError> {
         let guard = self.inner.lock().unwrap();
         if guard.reader_closed {
             return Err(TryAcquireError::ChannelClosed);
         }
 
-        try_get_slot::<true>(guard).map_err(|_| TryAcquireError::NotReady)
+        try_get_slot(guard).map_err(|_| TryAcquireError::NotReady)
     }
 }
 
@@ -159,30 +176,30 @@ mod test {
     use super::*;
     #[test]
     fn test_ringbuf_one_slot() {
-        let ringbuf = RingBuf::new(1);
+        let ringbuf = RingBuf::new(1, || 0);
         assert!(ringbuf.try_read().is_err());
-        assert!(ringbuf.try_write().is_ok());
+        *ringbuf.try_write().unwrap() = 1;
         assert!(ringbuf.try_write().is_err());
-        assert!(ringbuf.try_read().is_ok());
+        assert_eq!(*ringbuf.try_read().unwrap(), 1);
         assert!(ringbuf.try_read().is_err());
         assert!(ringbuf.try_write().is_ok());
     }
 
     #[test]
     fn test_ringbuf_multi_slot() {
-        let ringbuf = RingBuf::new(3);
+        let ringbuf = RingBuf::new(3, || 0);
         assert!(ringbuf.try_read().is_err());
-        assert!(ringbuf.try_write().is_ok());
-        assert!(ringbuf.try_write().is_ok());
-        assert!(ringbuf.try_write().is_ok());
+        *ringbuf.try_write().unwrap() = 1;
+        *ringbuf.try_write().unwrap() = 2;
+        *ringbuf.try_write().unwrap() = 3;
         assert!(ringbuf.try_write().is_err());
-        assert!(ringbuf.try_read().is_ok());
-        assert!(ringbuf.try_read().is_ok());
-        assert!(ringbuf.try_write().is_ok());
-        assert!(ringbuf.try_write().is_ok());
-        assert!(ringbuf.try_read().is_ok());
-        assert!(ringbuf.try_read().is_ok());
-        assert!(ringbuf.try_read().is_ok());
+        assert_eq!(*ringbuf.try_read().unwrap(), 1);
+        assert_eq!(*ringbuf.try_read().unwrap(), 2);
+        *ringbuf.try_write().unwrap() = 4;
+        *ringbuf.try_write().unwrap() = 5;
+        assert_eq!(*ringbuf.try_read().unwrap(), 3);
+        assert_eq!(*ringbuf.try_read().unwrap(), 4);
+        assert_eq!(*ringbuf.try_read().unwrap(), 5);
         assert!(ringbuf.try_read().is_err());
     }
 }
