a64774e4ab2686412a509952ef9c01f98283c245
Vincent Sparks <vince@spacestation.blue>
2024-09-13 13:20:17 -0700

    fleshed out the widget and rigged it up to a test app

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use std::sync::{atomic::Ordering, Arc};
 
-use ffmpeg_player::{self, ringbuf::RingBuf, DecodeThreadArgs, SynchronizationInfo};
+use egui::CentralPanel;
+use ffmpeg_player::{self, ringbuf::RingBuf, widget::VideoPlayerWidget, DecodeThreadArgs, SynchronizationInfo};
 use ffmpeg_the_third::frame::Video as VideoFrame;
+
+struct VideoPlayerApp {
+    widget: VideoPlayerWidget,
+}
+
+impl eframe::App for VideoPlayerApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            self.widget.show(ui);
+        });
+    }
+}
 
 fn main() {
     let f = std::fs::File::open("/home/vsparks/Videos/most fashionable faction.webm").unwrap();
     //let data = std::fs::read("/home/vsparks/Videos/lagtrain.webm").unwrap();
     //let f = std::io::Cursor::new(data);
-    let (video_sender, video_receiver) = oneshot::channel();
-    let synchronization_info: Arc<ffmpeg_player::SynchronizationInfo> = Default::default();
-    let sync_info2 = synchronization_info.clone();
-    synchronization_info.is_playing.store(true, Ordering::Relaxed);
-//    std::thread::spawn(move || {
+    let widget = VideoPlayerWidget::new(|args| {
         let mut cio = ffmpeg_player::CustomIO::new_read_seekable(f);
         let mut input = cio.open_input().unwrap();
-        ffmpeg_player::video_decode_thread(&mut input, DecodeThreadArgs {synchronization_info: sync_info2, video_sender});
- //   });
+        ffmpeg_player::video_decode_thread(&mut input, args);
+    });
+
+    let app = VideoPlayerApp {widget};
+
+    eframe::run_native("ffmpeg video player", eframe::NativeOptions::default(), Box::new(move |_| Ok(Box::new(app)))).unwrap();
 
     
 }
--- src/widget.rs
+++ src/widget.rs
@@ -1,5 +1,6 @@
-use std::sync::Arc;
+use std::sync::{atomic::Ordering, Arc};
 
+use egui::{load::SizedTexture, Color32, Pos2, Rect, Rounding, Sense, TextureOptions, Vec2};
 use ffmpeg_the_third::frame::Video as VideoFrame;
 
 use crate::{ringbuf::RingBuf, DecodeThreadArgs, SynchronizationInfo};
@@ -9,7 +10,7 @@ use pending::Pending;
 pub struct VideoPlayerWidget {
     video_queue: Pending<Arc<RingBuf<VideoFrame>>>,
     synchronization_info: Arc<SynchronizationInfo>,
-    texture: Option<egui::TextureId>,
+    texture: Option<egui::TextureHandle>,
 }
 
 impl VideoPlayerWidget {
@@ -29,18 +30,46 @@ impl VideoPlayerWidget {
         ui.add(self)
     }
     
-    fn draw_transport(&mut self, ui: &mut egui::Ui) {
-        
+    fn draw_playback_controls(&mut self, ui: &mut egui::Ui) {
+        let (play_button_resp, play_button_painter) = ui.allocate_painter(Vec2::new(20.0,20.0), Sense::click());
+        let is_playing = if play_button_resp.clicked() {
+            self.synchronization_info.is_playing.fetch_not(Ordering::AcqRel)
+        } else {
+            self.synchronization_info.is_playing.load(Ordering::Relaxed)
+        };
+        // TODO draw actual play and pause buttons.
+        if is_playing {
+            play_button_painter.rect_filled(Rect::from_min_size(Pos2::new(0.,0.),Vec2::new(20.,20.)),Rounding::ZERO,Color32::GREEN);
+        } else {
+            play_button_painter.rect_filled(Rect::from_min_size(Pos2::new(0.,0.),Vec2::new(20.,20.)),Rounding::ZERO,Color32::RED);
+        }
     }
 }
 
 impl egui::Widget for &mut VideoPlayerWidget {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
         ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
-            self.draw_transport(ui);
+            self.draw_playback_controls(ui);
             if let Some(video) = self.video_queue.try_load() {
                 if let Ok(frame) = video.read() {
-                    // show the video frame
+                    let pixels = frame.data(0)
+                        .array_chunks::<4>()
+                        .copied()
+                        .map(|[r,g,b,a]| Color32::from_rgba_unmultiplied(r,g,b,a))
+                        .collect::<Vec<_>>();
+                    let image = egui::ColorImage {
+                        size: [frame.width() as usize, frame.height() as usize],
+                        pixels
+                    };
+                    let options = TextureOptions::LINEAR;
+                    if let Some(ref mut texture) = self.texture {
+                        texture.set(image, options);
+                    } else {
+                        self.texture = Some(ui.ctx().load_texture("video", image, options));
+                    }
+                    ui.add(egui::Image::new(SizedTexture::from_handle(self.texture.as_ref().unwrap()))
+                        .maintain_aspect_ratio(true)
+                    );
                 } else {
                     // if we get here, video is at eof
                 }
